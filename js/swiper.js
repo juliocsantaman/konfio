95692914aeb7b4abcf2ce67ee18678bb130bdbd7
juliocsantaman <juliocsantaman@gmail.com>
2021-11-07 15:54:26 -0500

    Making client results.

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('.swiper1', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -7,9 +7,25 @@ const swiper = new Swiper('.swiper', {
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination1',
     type: 'bullets',
     clickable: true,
-    bulletClass: 'swiper-pagination-bullet'
+    // bulletClass: 'swiper-pagination-bullet'
+  },
+});
+
+const swiper2 = new Swiper('.swiper2', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  centeredSlides: true,
+  effect: 'slide', //flip
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination2',
+    type: 'bullets',
+    clickable: true,
+    // bulletClass: 'swiper-pagination-bullet'
   },
 });
