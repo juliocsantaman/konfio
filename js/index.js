2c7ca7b0f828ec1104e1ffa1ba3f78e2461d3101
juliocsantaman <juliocsantaman@gmail.com>
2021-11-10 13:34:18 -0500

    Making navbar for laptop and up.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 let containerBtnPurple = document.getElementById('container-btn-purple');
+let btnCreate = document.getElementById('btnCreate');
 
 window.addEventListener('scroll', (e) => {
   console.log(window.scrollY);
 
   if(window.scrollY == 0) {
     containerBtnPurple.classList.add('hidden');  
+    btnCreate.classList.add('hidden');
   }
 
   if(window.scrollY > 1) {
     containerBtnPurple.classList.remove('hidden');
+    btnCreate.classList.remove('hidden');
   }
 });
