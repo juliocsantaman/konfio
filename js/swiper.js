93af48acc36b44f45c12340220e558912a5404b2
juliocsantaman <juliocsantaman@gmail.com>
2021-11-10 19:59:45 -0500

    Doing client results for laptop.

--- js/swiper.js
+++ js/swiper.js
@@ -12,6 +12,15 @@ const swiper1 = new Swiper('.swiper1', {
     clickable: true,
     // bulletClass: 'swiper-pagination-bullet'
   },
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      // spaceBetween: 40
+    }
+  }
 });
 
 const swiper2 = new Swiper('.swiper2', {
@@ -24,6 +33,22 @@ const swiper2 = new Swiper('.swiper2', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination2',
+    type: 'bullets',
+    clickable: true,
+    // bulletClass: 'swiper-pagination-bullet'
+  },
+});
+
+const swiper3 = new Swiper('.swiper3', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  centeredSlides: true,
+  effect: 'slide', //flip
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination3',
     type: 'bullets',
     clickable: true,
     // bulletClass: 'swiper-pagination-bullet'
