4c7d252547caf1c49c235d97cc6427243f8767e2
juliocsantaman <juliocsantaman@gmail.com>
2021-12-06 18:37:07 -0500

    Making promotion section of business card page.

--- js/swiper.js
+++ js/swiper.js
@@ -223,3 +223,29 @@ const swiperAdditionalFunctionalities = new Swiper('.swiper-additional-functiona
     }
   }
 });
+
+const swiperPromotions = new Swiper('.swiper-promotions', {
+  // Optional parameters
+  direction: 'horizontal',
+  effect: 'slide',
+  autoHeight: true,
+  loop: true,
+  slidesPerView: 1,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      loop: false,
+      spaceBetween: 10
+    }
+  }
+});
