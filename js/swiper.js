8cb924741a3a5c2fdae3dc63fbd1bb4f7329be75
juliocsantaman <juliocsantaman@gmail.com>
2021-12-28 17:23:59 -0500

    Making podcast section of Konfío voices page.

--- js/swiper.js
+++ js/swiper.js
@@ -567,3 +567,29 @@ const swiperOurVideoChannel = new Swiper('.swiper-our-video-channel', {
     }
   }
 });
+
+const swiperPodcast = new Swiper('.swiper-podcast', {
+  // Optional parameters
+  direction: 'horizontal',
+  effect: 'slide',
+  autoHeight: true,
+  loop: true,
+  slidesPerView: 1,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 4,
+      loop: false,
+      spaceBetween: 15
+    }
+  }
+});
